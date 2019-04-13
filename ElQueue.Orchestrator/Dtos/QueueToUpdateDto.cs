4761067095bc61fb2e95e5ddfde027515a2a74c9
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-04-13 16:23:00 +0300

    add dtos for insert/update queue scenarios

---
+++ ElQueue.Orchestrator/Dtos/QueueToUpdateDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ElQueue.Orchestrator.QueueOrchestrator
+{
+    public class QueueToUpdateDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int AccountId { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public int TimeSlotDuration { get; set; }
+
+        public int TimeSlotNumber { get; set; }
+    }
+}
