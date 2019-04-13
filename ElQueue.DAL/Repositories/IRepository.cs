394d74504f621c44e4c9f4ae5cbe6d377e20f6f0
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-04-13 11:35:34 +0300

    set up base repository infrastructure

---
+++ ElQueue.DAL/Repositories/IRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ElQueue.DAL.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+
+        T Get(int id);
+
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate);
+
+        IEnumerable<T> Get(Expression<Func<T, bool>> predicate);
+
+        Task<IEnumerable<T>> GetAsync();
+
+        IEnumerable<T> Get();
+
+        Task CreateAsync(T entity);
+
+        void Create(T entity);
+
+        void Update(T entity);
+
+        Task DeleteAsync(int id);
+
+        void Delete(int id);
+
+        Task<bool> ExistsAsync(int id);
+
+    }
+}
---
+++ ElQueue.DAL/Repositories/RepositoryBase.cs
@@ -0,0 +1,37 @@
+﻿using ElQueue.DAL.Infrastructure;
+using System;
+
+namespace ElQueue.DAL.Repositories
+{
+    public abstract class RepositoryBase<T> : IDisposable where T : class
+    {
+        protected QueueContext _context;        
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException("Already disposed");
+            }
+
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        public virtual void DisposeFromUnitOfWork()
+        {
+            Dispose(false);
+        }
+    }
+
+}
