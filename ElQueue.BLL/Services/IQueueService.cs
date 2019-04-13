278d9a5a555bb481bccc1b67e71d313b050ed0da
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-04-13 16:22:26 +0300

    add business layer and queue service

---
+++ ElQueue.BLL/Services/IQueueService.cs
@@ -0,0 +1,11 @@
+﻿using ElQueue.DAL.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ElQueue.BLL.Services
+{
+    public interface IQueueService
+    {
+        Task<IEnumerable<Queue>> GetQueuesByAccount(int accountId);
+    }
+}
