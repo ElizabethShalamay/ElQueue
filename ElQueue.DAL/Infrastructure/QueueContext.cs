c9bfa36c77706e1aa7d1cd7c24a7038acb6e0db2
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-04-13 11:16:23 +0300

    add EF context

---
+++ ElQueue.DAL/Infrastructure/QueueContext.cs
@@ -0,0 +1,39 @@
+﻿using ElQueue.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElQueue.DAL.Infrastructure
+{
+    public class QueueContext : DbContext
+    {
+        public QueueContext(DbContextOptions<QueueContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<Queue> Queues { get; set; }
+
+        public DbSet<TimeSlot> TimeSlots { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().HasIndex(p => p.Name).IsUnique();
+            modelBuilder.Entity<User>().HasIndex(p => p.Name).IsUnique();
+            modelBuilder.Entity<Queue>().HasIndex(g => g.Name).IsUnique();
+
+            modelBuilder.Entity<Queue>().HasOne(queue => queue.Account)
+                .WithMany(g => g.Queues).Metadata.DeleteBehavior = DeleteBehavior.Restrict;
+
+            modelBuilder.Entity<TimeSlot>().HasOne(timeSlot => timeSlot.User)
+                .WithMany(user => user.TimeSlots).Metadata.DeleteBehavior = DeleteBehavior.Restrict;
+
+            modelBuilder.Entity<TimeSlot>().HasOne(timeSlot => timeSlot.Queue)
+                .WithMany(queue => queue.TimeSlots).Metadata.DeleteBehavior = DeleteBehavior.Restrict;
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
