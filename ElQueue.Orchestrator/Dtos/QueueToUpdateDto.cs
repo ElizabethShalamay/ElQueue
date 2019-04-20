e6896da290f745ab1e69f574754babda7b704e08
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-04-20 13:07:07 +0300

    add orchestration level

--- ElQueue.Orchestrator/Dtos/QueueToUpdateDto.cs
+++ ElQueue.Orchestrator/Dtos/QueueToUpdateDto.cs
@@ -1,23 +1,9 @@
 ﻿using System;
 
-namespace ElQueue.Orchestrator.QueueOrchestrator
+namespace ElQueue.Orchestrator.Dtos
 {
-    public class QueueToUpdateDto
+    public class QueueToUpdateDto : NewQueueDto
     {
         public int Id { get; set; }
-
-        public string Name { get; set; }
-
-        public int AccountId { get; set; }
-
-        public string Address { get; set; }
-
-        public DateTime StartTime { get; set; }
-
-        public DateTime EndTime { get; set; }
-
-        public int TimeSlotDuration { get; set; }
-
-        public int TimeSlotNumber { get; set; }
     }
 }
