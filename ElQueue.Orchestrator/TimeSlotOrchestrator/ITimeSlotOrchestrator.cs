a8c0dec714ad56fda651f2a81b360ae518427a41
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-04-20 18:03:29 +0300

    add time slot orchestrator

---
+++ ElQueue.Orchestrator/TimeSlotOrchestrator/ITimeSlotOrchestrator.cs
@@ -0,0 +1,22 @@
+﻿using ElQueue.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ElQueue.Orchestrator.TimeSlotOrchestrator
+{
+    public interface ITimeSlotOrchestrator
+    {
+        Task<TimeSlotBm> GetTimeSlotByIdAsync(int id);
+
+        Task<IEnumerable<TimeSlotBm>> GetAllTimeSlotsForQueueAsync(int queueId);
+
+        Task<bool> CheckIfTimeSlotExistsAsync(int id);
+
+        Task<IEnumerable<TimeSlotBm>> AddTimeSlotsAsync(DateTime start, DateTime end, int duration, int queueId);
+
+        Task ClearTimeSlotAsync(int id);
+
+        Task SelectSlotAsync(int id, int userId);
+    }
+}
