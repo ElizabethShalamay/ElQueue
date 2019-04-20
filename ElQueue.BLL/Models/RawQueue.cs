b7b2efb858b425ca88cc8695460a044da892f7c5
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-04-20 18:00:23 +0300

    add raw models for time slots

---
+++ ElQueue.BLL/Models/RawQueue.cs
@@ -0,0 +1,17 @@
+﻿namespace ElQueue.BLL.Models
+{
+    public class RawQueue
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int AccountId { get; set; }
+
+        public QueueAccountBm Account { get; set; }
+
+        public string Address { get; set; }
+    }
+}
