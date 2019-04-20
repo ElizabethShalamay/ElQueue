d669710e80d9d9bc3092c6cdf262d141bf09d291
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-04-20 18:03:08 +0300

    add time slot process service

---
+++ ElQueue.Web/Services/TimeSlotProcessService/ITimeSlotProcessService.cs
@@ -0,0 +1,12 @@
+﻿using ElQueue.Orchestrator.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ElQueue.Web.Services.TimeSlotProcessService
+{
+    public interface ITimeSlotProcessService
+    {
+        Task<ActionResult> ProcessTimeSlotAdditionAsync(TimeSlotTemplate template);
+        Task<ActionResult> ProcessTimeSlotSelectionAsync(int id, int userId);
+    }
+}
